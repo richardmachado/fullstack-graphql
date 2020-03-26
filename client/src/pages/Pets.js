4e832846fd9fe943314b656956a142d68140e621
Richard Machado <rich92105@gmail.com>
2020-03-25 18:45:12 -0700

    switch

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,11 +5,69 @@ import NewPet from '../components/NewPet'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import Loader from '../components/Loader'
 
+const PET_DETAILS = gql`
+  fragment PetDetails on Pet {
+    id
+    type
+    name
+    img
+    vacinated @client
+  }
+`
+
+const GET_PETS = gql`
+  query petsList($input: PetsInput) {
+    pets(input: $input) {
+      ...PetDetails
+    }
+  }
+  ${PET_DETAILS}
+`
+
+const CREATE_PET = gql`
+  mutation CreatePet($input: NewPetInput!) {
+    addPet(input: $input) {
+      ...PetDetails
+    }
+  }
+  ${PET_DETAILS}
+`;
+
 export default function Pets () {
   const [modal, setModal] = useState(false)
-  
+  const pets = useQuery(GET_PETS)
+
+  const [createPet, newPet] = useMutation(CREATE_PET, {
+    update(cache, { data: { addPet } }) {
+      const { pets } = cache.readQuery({ query: GET_PETS })
+
+      cache.writeQuery({
+        query: GET_PETS,
+        data: { pets: [addPet, ...pets] }
+      })
+    }
+  })
+
+  if (pets.loading) return <Loader />
+  if (pets.error || newPet.error) return <p>ERROR</p>
+
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: {input},
+    
+      optimisticResponse: {
+        __typename: 'Mutation',
+        addPet: {
+          __typename: 'Pet',
+          id: Math.round(Math.random() * -1000000) + '',
+          type: input.type,
+          name: input.name,
+          img: 'https://via.placeholder.com/300',
+          vacinated: true
+        }
+      }
+    })
   }
 
   const petsList = pets.data.pets.map(pet => (
